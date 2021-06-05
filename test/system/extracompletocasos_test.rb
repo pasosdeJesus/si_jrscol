3cb65c8122a2df3b8dc27a2b40b1eeb177a95d64
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-06-04 19:39:01 -0500

    caso de prueba que falla en adJ, veamos en travis?

---
+++ test/system/extracompletocasos_test.rb
@@ -0,0 +1,40 @@
+require 'application_system_test_case'
+
+class ExtracompletocasosTest < ApplicationSystemTestCase
+
+  setup do
+    @usuario = Usuario.find_by(nusuario: 'sjrcol')
+    @usuario.password = 'sjrcol123'
+    visit new_usuario_session_path 
+    fill_in "Usuario", with: @usuario.nusuario
+    fill_in "Clave", with: @usuario.password
+    click_button "Iniciar Sesión"
+    assert page.has_content?("Administrar")
+
+
+    # Creamos caso 
+    #visit '/casos/nuevo'
+    #@numcaso=find_field('Código').value
+
+    #cs = Sivel2Sjr::Casosjr.where(id_caso: @numcaso).take
+    #p = cs.contacto
+    #p.nombres = 'Juan'
+    #p.apellidos = 'Perez'
+    #p.save!
+  end
+
+  test "genera con usuario administrador en base inicial" do
+    assert page.has_content?("Casos")
+    click_on "Casos"
+    assert page.has_content?("Listado")
+    byebug
+    click_on "Listado"
+    byebug
+    assert page.has_content?("Búsqueda Avanzada")
+    click_on "Búsqueda Avanzada"
+    assert page.has_content?("Fecha de ingreso")
+
+  end
+
+end
+
