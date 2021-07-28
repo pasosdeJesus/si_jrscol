39bcc9c89dcba87fd743af032dc0c367f5522215
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-07-28 16:41:07 -0500

    Ajusta siglas

--- db/migrate/20210728173950_ajusta_siglas_doc.rb
+++ db/migrate/20210728173950_ajusta_siglas_doc.rb
@@ -3,12 +3,14 @@ class AjustaSiglasDoc < ActiveRecord::Migration[6.1]
     execute <<-SQL
       UPDATE sip_tdocumento SET sigla='CON' WHERE id=5;
       UPDATE sip_tdocumento SET sigla='CE' WHERE id=13;
+      UPDATE sip_tdocumento SET sigla='ANV' WHERE id=15;
     SQL
   end
   def down
     execute <<-SQL
       UPDATE sip_tdocumento SET sigla='CT' WHERE id=5;
       UPDATE sip_tdocumento SET sigla='' WHERE id=13;
+      UPDATE sip_tdocumento SET sigla='AN' WHERE id=15;
     SQL
   end
 end
