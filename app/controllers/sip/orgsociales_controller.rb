354e3df4338ae7c7f10dc3fa85d1a10137198dfd
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-08-31 16:11:01 -0500

    Actualiza a bootstrap 5. Closes #402

--- app/controllers/sip/orgsociales_controller.rb
+++ app/controllers/sip/orgsociales_controller.rb
@@ -1,10 +1,13 @@
-# encoding: UTF-8
 require_dependency "sip/concerns/controllers/orgsociales_controller"
 
 module Sip
   class OrgsocialesController < Heb412Gen::ModelosController
+
+    before_action :set_orgsocial, only: [:show, :edit, :update, :destroy]
+    load_and_authorize_resource class: Sip::Orgsocial
+
     include Sip::Concerns::Controllers::OrgsocialesController
-    
+
     def atributos_index
       [ :id, 
         :grupoper_id,
