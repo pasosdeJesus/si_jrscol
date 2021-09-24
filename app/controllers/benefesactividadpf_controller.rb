8cdd155b0f86ad723876d43d840abf2187c22295
Alejandro Cruz <alejocruzzz@gmail.com>
2021-09-24 11:58:35 -0500

    organiza y prepara para plantilla

--- app/controllers/benefesactividadpf_controller.rb
+++ app/controllers/benefesactividadpf_controller.rb
@@ -98,58 +98,9 @@ class BenefesactividadpfController < Heb412Gen::ModelosController
       "DROP MATERIALIZED VIEW IF EXISTS benefactividadpf")
     ActiveRecord::Base.connection.execute("
         CREATE MATERIALIZED VIEW benefactividadpf AS
-        #{subasis(lisp, lisa, contarb_listaac)}
+        #{Benefactividadpf.subasis(lisp, lisa, contarb_listaac)}
       ")
     Benefactividadpf.reset_column_information
   end # def crea_consulta
-
-
-  def subasis(lisp, lisa, actividadespf)
-    c="SELECT sub.*, 
-      (SELECT nombre FROM cor1440_gen_rangoedadac AS red
-       WHERE id=CASE 
-         WHEN (red.limiteinferior IS NULL OR 
-           red.limiteinferior<=sub.edad_en_actividad) AND 
-           (red.limitesuperior IS NULL OR
-           red.limitesuperior>=sub.edad_en_actividad) THEN
-           red.id
-         ELSE
-           7 -- SIN INFORMACION
-       END LIMIT 1) AS rangoedadac_nombre
-      FROM (SELECT p.id AS persona_id,
-       TRIM(TRIM(p.nombres) || ' '  ||
-         TRIM(p.apellidos)) AS persona_nombre,
-       public.sip_edad_de_fechanac_fecharef(
-         p.anionac, p.mesnac, p.dianac,
-       EXTRACT(YEAR FROM a.fecha)::integer,
-       EXTRACT(MONTH from a.fecha)::integer,
-       EXTRACT(DAY FROM a.fecha)::integer) AS edad_en_actividad,
-       TRIM(COALESCE(td.sigla || ':', '') ||
-         COALESCE(p.numerodocumento, '')) AS persona_identificacion,
-       p.sexo AS persona_sexo" 
-       if actividadespf.count > 0
-         c+= ", "   
-         actividadespf.each do |apf|
-           cod = apf.objetivopf.numero + apf.resultadopf.numero + apf.nombrecorto
-           c+='(
-                 SELECT COUNT(*) FROM cor1440_gen_asistencia AS asistencia
-                 JOIN cor1440_gen_actividad_actividadpf AS aapf ON aapf.actividad_id=asistencia.actividad_id
-                 WHERE aapf.actividadpf_id = ' + apf.id.to_s + '  
-                 AND persona_id = p.id
-                 AND asistencia.actividad_id IN (' + lisa + ') 
-                 ) AS "'+ cod +'"' 
-                  if apf != actividadespf.last
-                    c += ', '
-                  end
-          end
-        end
-       c+="
-       FROM sip_persona AS p
-     JOIN cor1440_gen_asistencia AS asis ON asis.persona_id=p.id 
-     LEFT JOIN sip_tdocumento AS td ON td.id=p.tdocumento_id
-     JOIN cor1440_gen_actividad AS a ON asis.actividad_id=a.id
-     WHERE p.id IN (#{lisp}) GROUP BY persona_identificacion, p.id, edad_en_actividad) AS sub
-    "
-  end
 
 end
