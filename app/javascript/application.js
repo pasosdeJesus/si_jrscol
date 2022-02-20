f4aa3f63ca0f236c23ed4190ac6131704c0b8775
Nombre Usuario <correo@usuario.net>
2022-02-20 09:37:29 -0500

    Reorganiza y carga stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@
 console.log('Hola Mundo desde Webpacker')
 
 import Rails from "@rails/ujs";
-import "@hotwired/turbo-rails";
 Rails.start();
 window.Rails = Rails
 
@@ -25,37 +24,7 @@ import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
 var L = require('leaflet');
 var mc= require('leaflet.markercluster');
 
-document.addEventListener('change', 
-  function (event) {
-    var m = event.target.id.match(/^caso_migracion_attributes_([0-9]*)_perfilmigracion_id$/)
-    if (m != null) {
-      var pd = document.getElementById('caso_migracion_attributes_' + m[1] +
-        '_destino_pais_id').parentElement;
-      var dd = document.getElementById('caso_migracion_attributes_' + m[1] +
-        '_destino_departamento_id').parentElement;
-      var md = document.getElementById('caso_migracion_attributes_' + m[1] +
-        '_destino_municipio_id').parentElement;
-      var cd = document.getElementById('caso_migracion_attributes_' + m[1] +
-        '_destino_clase_id').parentElement;
-      var fd = document.getElementById('caso_migracion_attributes_' + m[1] +
-        '_fechaendestino').parentElement;
-      if (event.target.checked) {
-        pd.style.display = 'none'
-        dd.style.display = 'none'
-        md.style.display = 'none'
-        cd.style.display = 'none'
-        fd.style.display = 'none'
-      } else {
-        pd.style.display = ''
-        dd.style.display = ''
-        md.style.display = ''
-        cd.style.display = ''
-        fd.style.display = ''
-      }
-
-    }
-  }
-)
+import "./caso_m"
 
 // https://turbo.hotwired.dev/handbook/building
 // dice
@@ -135,3 +104,4 @@ document.addEventListener('turbo:load', (e) => {
   sip_ejecutarAlCargarPagina(window)
 })
 
+import "./controllers"
