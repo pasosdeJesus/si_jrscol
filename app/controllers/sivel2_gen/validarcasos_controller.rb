c96c4a6ca03d584e61423fc05965e45546aa4cf9
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-04-29 16:52:07 -0500

    repetidos ya no esta en validar casos

--- app/controllers/sivel2_gen/validarcasos_controller.rb
+++ app/controllers/sivel2_gen/validarcasos_controller.rb
@@ -8,54 +8,6 @@ module Sivel2Gen
     include Sivel2Sjr::Concerns::Controllers::ValidarcasosController
 
 
-
-    def filtro_benef_fechas(benef, cfecha = 'sip_persona.created_at')
-      pfid = ''
-      if (params[:validarcaso] && params[:validarcaso][:fechaini] && 
-          params[:validarcaso][:fechaini] != '')
-        pfi = params[:validarcaso][:fechaini]
-        pfid = Sip::FormatoFechaHelper.fecha_local_estandar pfi
-      else
-        # Comenzar en semestre anterior
-        pfid = Sip::FormatoFechaHelper.inicio_semestre(Date.today).to_s
-      end
-      benef = benef.where("#{cfecha} >= ?", pfid)
-      if(params[:validarcaso] && params[:validarcaso][:fechafin] && 
-          params[:validarcaso][:fechafin] != '')
-        pff = params[:validarcaso][:fechafin]
-        pffd = Sip::FormatoFechaHelper.fecha_local_estandar pff
-        if pffd
-          benef = benef.where("#{cfecha} <= ?", pffd)
-        end
-      end
-      return benef
-    end
-
-    def validar_idrepetida
-      benef = Sip::Persona.all
-      puts "OJO 1 benef.count=#{benef.count}"
-      benef = filtro_benef_fechas(benef)
-      res="SELECT sub.sigla, sub.numerodocumento, sub.rep,\n"\
-        "  ARRAY(SELECT id FROM (" + benef.to_sql + ") AS p2\n"\
-        "        WHERE p2.tdocumento_id=sub.tdocumento_id\n"\
-        "        AND p2.numerodocumento=sub.numerodocumento)\n"\
-        "  FROM (SELECT t.sigla, p.tdocumento_id, numerodocumento,\n"\
-        "      COUNT(p.id) AS rep "\
-        "      FROM (" + benef.to_sql + ") AS p\n"\
-        "      LEFT JOIN sip_tdocumento as t ON t.id=tdocumento_id\n"\
-        "      GROUP BY 1,2,3) AS sub\n"\
-        "  WHERE rep>1\n"\
-        "  ORDER BY rep DESC";
-      arr = ActiveRecord::Base.connection.select_all(res)
-      @validaciones << { 
-        titulo: 'Identificaciones repetidas de beneficiarios actualizados en el rango de fechas',
-        encabezado: ['Tipo de documento', 'Núm. documento', 'Num. repetidos', 
-                     'Identificaciones'],
-        cuerpo: arr 
-      }
-    end
-
-
     def validar_sinderechovulnerado
       casos = ini_filtro
       casos = casos.joins('JOIN sivel2_sjr_respuesta ON
@@ -88,7 +40,6 @@ module Sivel2Gen
 
     def validar_interno
       @rango_fechas = 'Fecha de recepción'
-      validar_idrepetida
       validar_sivel2_sjr
       validar_sinderechovulnerado
       validar_sivel2_gen
