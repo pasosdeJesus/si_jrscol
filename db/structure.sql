4cd49ddd3b09efdbdf56bf733f5060784f449707
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-06-14 13:03:55 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -6351,7 +6351,7 @@ CREATE MATERIALIZED VIEW public.sivel2_gen_consexpcaso AS
      LEFT JOIN public.sivel2_sjr_ultimaatencion ultimaatencion ON ((ultimaatencion.caso_id = caso.id)))
   WHERE (conscaso.caso_id IN ( SELECT sivel2_gen_conscaso.caso_id
            FROM public.sivel2_gen_conscaso
-          WHERE (sivel2_gen_conscaso.caso_id = 137)
+          WHERE (sivel2_gen_conscaso.caso_id = 105)
           ORDER BY sivel2_gen_conscaso.fecharec DESC, sivel2_gen_conscaso.caso_id))
   ORDER BY conscaso.fecha, conscaso.caso_id
   WITH NO DATA;
