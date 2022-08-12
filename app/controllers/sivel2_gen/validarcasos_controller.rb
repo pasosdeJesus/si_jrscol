3d8f7b1345f31d2b469314b74ed203e8b2987597
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-08-12 17:36:29 -0500

    Ampliar Casos->Validación con listado de casos parcialmente eliminados. Closes #644

--- app/controllers/sivel2_gen/validarcasos_controller.rb
+++ app/controllers/sivel2_gen/validarcasos_controller.rb
@@ -8,7 +8,7 @@ module Sivel2Gen
     include Sivel2Sjr::Concerns::Controllers::ValidarcasosController
 
 
-    def validar_sinderechovulnerado
+    def validar_sin_derechovulnerado
       casos = ini_filtro
       casos = casos.joins('JOIN sivel2_sjr_respuesta ON
               sivel2_sjr_respuesta.id_caso=sivel2_sjr_casosjr.id_caso')
@@ -19,6 +19,29 @@ module Sivel2Gen
                (SELECT id_respuesta FROM public.sivel2_sjr_derecho_respuesta)'
       )
     end
+
+    def validar_sin_casosjr
+      casos = Sivel2Gen::Caso.order(:id)
+      casos = filtro_fechas(casos, 'fecha')
+      casos = filtro_etiqueta(casos)
+      atr = ['sivel2_gen_caso.id', 
+              'sivel2_gen_caso.fecha' ]
+      encabezado = [
+        'Código', 'Fecha de Desp. Emb.', 
+        'Asesor']
+      where = 'sivel2_gen_caso.id NOT IN 
+           (SELECT id_caso FROM public.sivel2_sjr_casosjr) '
+      titulo = 'Casos parcialmente eliminados'
+      res = casos.where(where).select(atr)
+      puts "validacion_estandar: res.to_sql=", res.to_sql
+      arr = ActiveRecord::Base.connection.select_all(res.to_sql)
+      @validaciones << { 
+        titulo: titulo,
+        encabezado: encabezado,
+        cuerpo: arr 
+      }
+    end
+
 
     ## Sobrecarga una de sivel2_sjr 
     def valida_sinayudasjr
@@ -40,8 +63,9 @@ module Sivel2Gen
 
     def validar_interno
       @rango_fechas = 'Fecha de recepción'
+      validar_sin_casosjr
       validar_sivel2_sjr
-      validar_sinderechovulnerado
+      validar_sin_derechovulnerado
       validar_sivel2_gen
     end # def validar_interno
          
