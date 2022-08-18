b846de4be136168b1582c73cd0390d3740aa40d2
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-08-18 09:24:10 -0500

    Mejorad migracion para reformatear observaciones

--- db/migrate/20220818133338_cambia_observacion_etiqueta_persona.rb
+++ db/migrate/20220818133338_cambia_observacion_etiqueta_persona.rb
@@ -2,7 +2,36 @@ class CambiaObservacionEtiquetaPersona < ActiveRecord::Migration[7.0]
   def up
     execute <<-SQL
       UPDATE sip_etiqueta_persona 
-        SET observaciones = replace(observaciones, ';', E'\n');
+        SET observaciones = replace(observaciones, '; ', E'\n');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Nombres:', '* Nombres:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Apellidos:', '* Apellidos:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Tipo doc.:', '* Tipo doc.:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Número de doc.:', '* Número de doc.:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Año nac.:', '* Año nac.:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Mes nac.:', '* Mes nac.:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Dia nac.:', '* Dia nac.:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Sexo nac.:', '* Sexo nac.:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Pais nac.:', '* Pais nac.:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Departamento nac.:', '* Departamento nac.:');
+        SET observaciones = replace(observaciones, 'Muncipio nac.:', '* Municipio nac.:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Centro poblado nac.:', '* Centro poblado nac.:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Nacional de:', '* Nacional de:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Fecha creación:', '* Fecha creación:');
+      UPDATE sip_etiqueta_persona 
+        SET observaciones = replace(observaciones, 'Fecha actualización:', '* Fecha actualización:');
     SQL
   end
 
