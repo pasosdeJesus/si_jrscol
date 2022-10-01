5f8c6ddb4852b3bc48c3d8d082d26c839a63abae
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-10-01 00:21:02 -0500

    Mejora migracion

--- db/migrate/20221001042722_elimbenefext.rb
+++ db/migrate/20221001042722_elimbenefext.rb
@@ -1,12 +1,12 @@
 class Elimbenefext < ActiveRecord::Migration[7.0]
   def up
     execute <<-SQL
-      DROP VIEW benefext CASCADE;
+      DROP VIEW IF EXISTS benefext CASCADE;
     SQL
   end
   def down
     execute <<-SQL
-      DROP VIEW benefext CASCADE;
+      DROP VIEW IF EXISTS benefext CASCADE;
     SQL
   end
 
