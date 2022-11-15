4e488a0964a898c95015d04d018f53236b8dead9
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-11-15 05:33:53 -0500

    Arregla personas en base con fecha de nacimiento errada por ser 29.Feb de un año no bisiesto --les pone 28

--- db/migrate/20221115102504_arregla_29_en_no_bisiesto.rb
+++ db/migrate/20221115102504_arregla_29_en_no_bisiesto.rb
@@ -3,7 +3,7 @@ class Arregla29EnNoBisiesto < ActiveRecord::Migration[7.0]
     derr = Sip::Persona.where(dianac: 29).
       where(mesnac: 2).
       where('anionac%4<>0 AND anionac%1000<>0')
-    puts "Se encontraron y cambiaron #{derr.count} personas con fecha de nacimiento 29.Feb de un año no bisiesto"
+    puts "Se encontraron y cambiaron #{derr.count} personas con fecha de nacimiento 29.Feb de un año no bisiesto (#{derr.pluck(:id).join(', ')})"
     execute <<-SQL
       UPDATE sip_persona SET dianac=28 WHERE
         dianac=29 AND mesnac=2 AND anionac%4<>0 AND anionac%1000<>0 ;
