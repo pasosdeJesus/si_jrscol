9ab147a9bc2a42011019124623f62a3d2820a467
Luis Alejandro Cruz <alejocruzzz@gmail.com>
2022-11-17 00:20:52 -0500

    rol familia solo en nucleo familiar no en contacto y agrega pareja a rolfamilia opciones

---
+++ db/migrate/20221117050734_agrega_pareja_a_rolfamilia.rb
@@ -0,0 +1,15 @@
+class AgregaParejaARolfamilia < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      INSERT INTO public.sivel2_sjr_rolfamilia (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at)
+        VALUES (9, 'PAREJA', null, '2022-11-16', null, '2022-11-16', '2022-11-16');
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DELETE FROM public.sivel2_sjr_rolfamilia WHERE id=9
+    SQL
+  end
+
+end
