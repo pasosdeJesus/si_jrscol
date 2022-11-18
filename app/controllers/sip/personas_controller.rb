6fb07a904fd49aca9d7b69b8676195164c14aea1
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-11-18 07:56:04 -0500

    Mezcla

--- app/controllers/sip/personas_controller.rb
+++ app/controllers/sip/personas_controller.rb
@@ -29,6 +29,118 @@ module Sip
       return a
     end
 
+    def vistas_manejadas
+      ['Persona']
+    end
+
+    # Busca y lista persona(s)
+
+    def index(c = nil)
+      if c == nil
+        c = Sip::Persona.all
+      end
+      if params[:term]
+        term = Sivel2Gen::Caso.connection.quote_string(params[:term])
+        consNomvic = term.downcase.strip #sin_tildes
+        consNomvic.gsub!(/ +/, ":* & ")
+        if consNomvic.length > 0
+          consNomvic += ":*"
+        end
+        where = " persona.buscable @@ "\
+          "to_tsquery('spanish', '#{consNomvic}')";
+
+        partes = [
+          'nombres',
+          'apellidos',
+          'COALESCE(numerodocumento::TEXT, \'\')'
+        ]
+        s = "";
+        l = " persona.id ";
+        seps = "";
+        sepl = " || ';' || ";
+        partes.each do |p|
+          s += seps + p;
+          l += sepl + "char_length(#{p})";
+          seps = " || ' ' || ";
+        end
+        qstring = "SELECT TRIM(#{s}) AS value, #{l} AS id " +
+          "FROM public.sip_persona AS persona " +
+          "WHERE #{where} ORDER BY 1 LIMIT 20"
+        r = ActiveRecord::Base.connection.select_all qstring
+        respond_to do |format|
+          format.json { render :json, inline: r.to_json }
+          format.html { render :json, inline: 'No responde con parametro term' }
+        end
+      else
+        super(c)
+      end
+    end
+
+    # Están listas @persona, @victima, @personaant, @caso
+    # Y está listo para salvar la nueva persona @persona en
+    # @victima --remplazando @personaant.
+    # Continúa si esta función retorna true, de lo contrario
+    # se espera que la función haga render json con el error
+    # y que retorne false.
+    def remplazar_antes_salvar_v
+      ce = Sivel2Sjr::Casosjr.where(contacto: @persona.id)
+      if ce.count > 0
+        render json: "Ya es contacto en el caso #{ce.take.id_caso}.",
+          status: :unprocessable_entity
+        return false
+      end
+      ve = Sivel2Sjr::Victimasjr.joins('JOIN sivel2_gen_victima ' +
+                                       ' ON sivel2_gen_victima.id = sivel2_sjr_victimasjr.id_victima').
+                                       where('sivel2_gen_victima.id_persona' => @persona.id).
+                                       where(fechadesagregacion: nil)
+      if ve.count > 0
+        render json: "Está en núcleo familiar sin desagregar " +
+          "en el caso #{ve.take.victima.id_caso}", 
+          status: :unprocessable_entity
+        return false
+      end
+      # Si se está remplazando el contacto, borra la persona
+      # vacía que era contacto --y por lo mismo sólo permite 
+      # cuando es un contacto vacío.
+      if @caso.casosjr.contacto && @personaant &&
+          @caso.casosjr.contacto_id == @personaant.id 
+        eliminar_persona = false
+        if @caso.casosjr.contacto.nombres == ""
+          eliminar_persona = true
+        end
+        ppb=@caso.casosjr.contacto_id
+        @caso.casosjr.contacto_id = nil
+        @caso.casosjr.save!(validate: false)
+        vic = @caso.victima.where(id_persona: ppb).take
+        vic.id_persona=@persona.id
+        vic.save(validate: false)
+        @caso.casosjr.contacto_id = @persona.id
+        @caso.casosjr.save!(validate: false)
+        #redirect_to sivel2_gen.edit_caso_path(@caso)
+        begin
+          if eliminar_persona
+            @personaant.destroy
+          end
+          render partial: '/sip/personas/remplazar', layout: false
+        rescue
+        end
+        return false # buscar obligar el redirect_to
+      end
+
+      return true
+    end
+
+    def remplazar_despues_salvar_v
+      if @caso.casosjr.contacto.id == @personaant.id
+        @caso.casosjr.contacto = @persona
+        @caso.casosjr.save
+        if @caso.validate
+          @caso.save
+        end
+      end
+      return true
+    end
+
     def new
       @encform_html = {
         'data-controller': 'sindocaut'
