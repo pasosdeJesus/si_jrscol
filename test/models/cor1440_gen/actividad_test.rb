fdee635f7750ac1cd282dadf7c5c1c1e8b81a5a5
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-26 23:29:16 -0500

    Actualiza

--- test/models/cor1440_gen/actividad_test.rb
+++ test/models/cor1440_gen/actividad_test.rb
@@ -14,18 +14,37 @@ module Cor1440Gen
     end
 
     test "valido" do
+
+      assert_equal 1, Msip::Pais.where(id: 100).count
+      pais = Msip::Pais.find(100)
+      ubicacionpre = Msip::Ubicacionpre.create(PRUEBA_UBICACIONPRE2)
+
+      assert_predicate ubicacionpre, :valid?
+      assert_equal ubicacionpre.pais_id, pais.id
+
       a = Cor1440Gen::Actividad.new PRUEBA_ACTIVIDAD
       a.save(validate: false)
 
       ActividadProyectofinanciero.create(
         actividad_id: a.id,
-        financiador_id: 10 # PLAN ESTRATEGICO 1
+        proyectofinanciero_id: 10 # PLAN ESTRATEGICO 1
       ) 
-      ActividadAcitividadpf.create(
+      ActividadActividadpf.create(
         actividad_id: a.id,
         actividadpf_id: 116 # ASISTENCIA HUMANITARIA
       )
-      debugger
+      ap = ActividadProyecto.create!(
+        actividad_id: a.id,
+        proyecto_id: 101 # ACCIÓN HUMANITARIA
+      )
+      assert_predicate ap, :valid?
+
+      a.ubicacionpre_id = ubicacionpre.id
+      a.resultado = "res"
+      
+      assert a.valid?
+      
+
       assert a.valid?
       a.destroy
     end
