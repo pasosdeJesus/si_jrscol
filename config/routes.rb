a72ebc5af58a75b63cd0c97f6652ad94645f0583
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-29 09:36:48 -0500

    Rutas para iniciar y termina sesión en español

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
       get 'sign_out' => 'devise/sessions#destroy'
       get 'salir' => 'devise/sessions#destroy',
         as: :terminar_sesion
-
       post 'usuarios/iniciar_sesion', to: 'devise/sessions#create'
       get 'usuarios/iniciar_sesion', to: 'devise/sessions#new',
         as: :iniciar_sesion
