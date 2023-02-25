e824670a9b8cdcefcacce528e1b3e910272b5cc4
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-02-24 21:31:13 -0500

    Quita plantillas para generar reporte gifmm de forma que solo quede Reporte GIFMM (Excel). Closes #840

---
+++ db/migrate/20230225022307_quita_gifmm_no_excel.rb
@@ -0,0 +1,30 @@
+class QuitaGifmmNoExcel < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      DELETE FROM heb412_gen_campoplantillahcm WHERE
+        plantillahcm_id IN (
+          SELECT id FROM heb412_gen_plantillahcm 
+          WHERE vista='Consgifmm' AND id<>51
+        )
+      ;
+
+      DELETE FROM cor1440_gen_plantillahcm_proyectofinanciero WHERE
+        plantillahcm_id IN (
+          SELECT id FROM heb412_gen_plantillahcm 
+          WHERE vista='Consgifmm' AND id<>51
+        )
+      ;
+      DELETE FROM heb412_gen_formulario_plantillahcm WHERE
+        plantillahcm_id IN (
+          SELECT id FROM heb412_gen_plantillahcm 
+          WHERE vista='Consgifmm' AND id<>51
+        )
+      ;
+
+      DELETE FROM heb412_gen_plantillahcm WHERE vista='Consgifmm' AND id<>51;
+    SQL
+  end
+
+  def down
+  end
+end
