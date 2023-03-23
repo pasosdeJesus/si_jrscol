82ad0d8c6a5791a70312adc0f175cbb55e6b830a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-03-22 21:18:49 -0500

    Pone Migrante con vocación de permanencia en asistentes autocompletados que no tienen un perfil. Closes #584

--- app/controllers/cor1440_gen/actividades_controller.rb
+++ app/controllers/cor1440_gen/actividades_controller.rb
@@ -465,7 +465,7 @@ module Cor1440Gen
           asistencia = Cor1440Gen::Asistencia.create(
             actividad_id: act.id,
             persona_id: v.victima.id_persona,
-            perfilorgsocial_id: v.victima.persona.ultimoperfilorgsocial_id
+            perfilorgsocial_id: v.victima.persona.ultimoperfilorgsocial_id || 10
           )
           if !asistencia.save
             resp_error 'No pudo crear asistencia' 
