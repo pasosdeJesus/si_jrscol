3bf708991e147b8a5ffb6b48b2e719c8cc223bde
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-03-27 11:34:15 -0500

    iniciando convención de sexo cambiando en bd

---
+++ db/migrate/20230327161238_convsexo_mho.rb
@@ -0,0 +1,23 @@
+class ConvsexoMho < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      ALTER TABLE msip_persona DROP CONSTRAINT IF EXISTS persona_sexo_check;
+      UPDATE msip_persona SET sexo='H' WHERE sexo='M'; 
+      UPDATE msip_persona SET sexo='M' WHERE sexo='F';
+      UPDATE msip_persona SET sexo='O' WHERE sexo='S';
+      ALTER TABLE msip_persona ADD CONSTRAINT persona_sexo_check
+        CHECK (sexo = 'M' OR sexo = 'H' OR sexo = 'O');
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      ALTER TABLE msip_persona DROP CONSTRAINT IF EXISTS persona_sexo_check;
+      UPDATE msip_persona SET sexo='F' WHERE sexo='M'; 
+      UPDATE msip_persona SET sexo='M' WHERE sexo='H';
+      UPDATE msip_persona SET sexo='S' WHERE sexo='O';
+      ALTER TABLE msip_persona ADD CONSTRAINT persona_sexo_check
+        CHECK (sexo = 'F' OR sexo = 'M' OR sexo = 'S');
+    SQL
+  end
+end
