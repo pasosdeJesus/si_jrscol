d395932c8ae86c193d396b51c29e1a4f1dc877da
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-03-29 06:29:55 -0500

    Mejora validaciones y refactoriza persona. Closes #860

--- app/models/sivel2_gen/caso.rb
+++ app/models/sivel2_gen/caso.rb
@@ -1,35 +1,49 @@
-require 'sivel2_sjr/concerns/models/caso'
+require 'sivel2_gen/concerns/models/caso'
 class Sivel2Gen::Caso < ActiveRecord::Base
-  include Sivel2Sjr::Concerns::Models::Caso
+  include Sivel2Gen::Concerns::Models::Caso
+
+  has_many :actosjr, class_name: 'Sivel2Sjr::Actosjr',
+    :through => :acto, dependent: :destroy
+  accepts_nested_attributes_for :actosjr, allow_destroy: true, 
+    reject_if: :all_blank
+
+  has_one :casosjr, class_name: 'Sivel2Sjr::Casosjr',
+    foreign_key: "id_caso", inverse_of: :caso, validate: true, 
+    dependent: :destroy
+  accepts_nested_attributes_for :casosjr, allow_destroy: true, 
+    update_only: true
+
+  # respuesta deberìa ser con :through => :casosjr pero más dificil guardar
+  has_many :respuesta, class_name: 'Sivel2Sjr::Respuesta',
+    foreign_key: "id_caso", validate:true, dependent: :destroy
+  accepts_nested_attributes_for :respuesta, allow_destroy: true, 
+    reject_if: :all_blank
+
+  has_many :desplazamiento, class_name: 'Sivel2Sjr::Desplazamiento',
+    foreign_key: "id_caso", validate: true, dependent: :destroy
+  accepts_nested_attributes_for :desplazamiento , allow_destroy: true, 
+    reject_if: :all_blank
+  has_many :victimasjr, class_name: 'Sivel2Sjr::Victimasjr',
+    :through => :victima, dependent: :destroy
+  accepts_nested_attributes_for :victimasjr, allow_destroy: true, 
+    reject_if: :all_blank
 
   has_many :migracion, class_name: 'Sivel2Sjr::Migracion',
     foreign_key: "caso_id", validate: true, dependent: :destroy
   accepts_nested_attributes_for :migracion, allow_destroy: true, 
     reject_if: :all_blank
 
-  def presenta(atr)
-    case atr.to_s
-    when 'fecharec'
-      casosjr.fecharec if casosjr && casosjr.fecharec
-    when 'oficina'
-      casosjr.oficina.nombre if casosjr && casosjr.oficina
-    when 'asesor'
-      casosjr.usuario.nusuario if casosjr && casosjr.usuario
-    when 'contacto'
-      if casosjr && casosjr.contacto
-        casosjr.contacto.nombres + ' ' + casosjr.contacto.apellidos +
-       ' ' + ((casosjr.contacto.tdocumento.nil? ||
-               casosjr.contacto.tdocumento.sigla.nil?) ? '' : 
-              casosjr.contacto.tdocumento.sigla) + ' ' +
-       ' ' + (casosjr.contacto.numerodocumento.nil? ? '' : 
-              casosjr.contacto.numerodocumento)
+
+  validate :beneficiarios_con_ultimoperfilorgsocial
+  def beneficiarios_con_ultimoperfilorgsocial
+    victima.each do |v|
+      if v.persona.ultimoperfilorgsocial_id.nil?
+        errors.add(:persona, "Falta perfil poblacional de #{v.persona.presenta_nombre}")
       end
-    when 'direccion'
-      casosjr.direccion if casosjr && casosjr.direccion
-    when 'telefono'
-      casosjr.telefono if casosjr && casosjr.telefono
-    else
-      presenta_gen(atr) 
+      if v.persona.ultimoestatusmigratorio_id.nil?
+        errors.add(:persona, "Falta estatus migratorio de #{v.persona.presenta_nombre}")
+      end
+
     end
   end
 
@@ -56,6 +70,33 @@ class Sivel2Gen::Caso < ActiveRecord::Base
         errors.add(:migracion, 
                    "Si el migrante tiene PPT debe especificar el número")
       end
+    end
+  end
+
+
+  def presenta(atr)
+    case atr.to_s
+    when 'fecharec'
+      casosjr.fecharec if casosjr && casosjr.fecharec
+    when 'oficina'
+      casosjr.oficina.nombre if casosjr && casosjr.oficina
+    when 'asesor'
+      casosjr.usuario.nusuario if casosjr && casosjr.usuario
+    when 'contacto'
+      if casosjr && casosjr.contacto
+        casosjr.contacto.nombres + ' ' + casosjr.contacto.apellidos +
+          ' ' + ((casosjr.contacto.tdocumento.nil? ||
+                  casosjr.contacto.tdocumento.sigla.nil?) ? '' : 
+                 casosjr.contacto.tdocumento.sigla) + ' ' +
+                ' ' + (casosjr.contacto.numerodocumento.nil? ? '' : 
+                       casosjr.contacto.numerodocumento)
+      end
+    when 'direccion'
+      casosjr.direccion if casosjr && casosjr.direccion
+    when 'telefono'
+      casosjr.telefono if casosjr && casosjr.telefono
+    else
+      presenta_gen(atr) 
     end
   end
 
--- app/models/sivel2_sjr/casosjr.rb
+++ app/models/sivel2_sjr/casosjr.rb
@@ -1,39 +1,91 @@
-require 'sivel2_sjr/concerns/models/casosjr'
-
 class Sivel2Sjr::Casosjr < ActiveRecord::Base
-  include Sivel2Sjr::Concerns::Models::Casosjr
 
+  include Msip::Modelo
+
+  self.primary_key = :id_caso
+
+  # Ordenados por foreign_key para comparar con esquema en base
+  belongs_to :usuario, class_name: "Usuario", 
+    foreign_key: "asesor", validate: true, optional: false
+  belongs_to :comosupo, class_name: "Sivel2Sjr::Comosupo", 
+    foreign_key: "comosupo_id", validate: true, optional: true
+  belongs_to :contacto, class_name: "Msip::Persona",  
+    foreign_key: "contacto_id", optional: false
+  belongs_to :caso, class_name: "Sivel2Gen::Caso", validate: true,
+    foreign_key: "id_caso", inverse_of: :casosjr, optional: false
+  belongs_to :categoria, class_name: "Sivel2Gen::Categoria", 
+    validate: true, foreign_key: "categoriaref", optional: true
+  belongs_to :idioma, class_name: "Sivel2Sjr::Idioma", 
+    foreign_key: "id_idioma", validate: true, optional: true
+  belongs_to :llegada, class_name: "Msip::Ubicacion", validate: true,
+    foreign_key: "id_llegada", optional: true
   belongs_to :llegadam, class_name: 'Msip::Ubicacion', validate: true,
     foreign_key: 'id_llegadam', optional: true
+  belongs_to :proteccion, class_name: "Sivel2Sjr::Proteccion", 
+    foreign_key: "id_proteccion", validate: true, optional: true
+  belongs_to :oficina, class_name: "Msip::Oficina", 
+    foreign_key: "oficina_id", validate: true, optional: true
+  belongs_to :salida, class_name: "Msip::Ubicacion", validate: true,
+    foreign_key: "id_salida", optional: true
   belongs_to :salidam, class_name: 'Msip::Ubicacion', validate: true,
     foreign_key: 'id_salidam', optional: true
+  belongs_to :statusmigratorio, 
+    class_name: "Sivel2Sjr::Statusmigratorio", 
+    foreign_key: "id_statusmigratorio", validate: true, optional: true
+
+  has_many :actividad_casosjr, 
+    class_name: 'Sivel2Sjr::ActividadCasosjr',
+    validate: true, 
+    foreign_key: :casosjr_id
+  has_many :actividad, through: :actividad_casosjr
+  accepts_nested_attributes_for :actividad_casosjr, 
+    reject_if: :all_blank
+  accepts_nested_attributes_for :actividad, reject_if: :all_blank
 
   has_many :asesorhistorico,
     class_name: '::Asesorhistorico',
     validate: true, 
     foreign_key: :casosjr_id
 
-  validates :motivom, length: { maximum: 5000 }
-  validates :memo1612, length: { maximum: 5000 }
+  has_many :respuesta, class_name: "Sivel2Sjr::Respuesta", 
+    validate: true, foreign_key: "id_caso"#, dependent: :destroy
+
+  has_many :victima, class_name: 'Sivel2Gen::Victima', validate: true,
+    through: :caso
+
+  has_many :victimasjr, class_name: 'Sivel2Sjr::Victimasjr', validate: true,
+    through: :victima
+
+  
+  validates :asesor, presence: true
+  #_presence_of :asesor
   validates :contacto, uniqueness: { 
     message: 'Contacto no puede estar repetido en dos casos' 
   }
-
-  validates :estatus_refugio, length: { maximum: 5000 }
   validates :docrefugiado, length: { maximum: 128 }
+  validates :estatus_refugio, length: { maximum: 5000 }
+  validates :fecharec, presence: true
+  #_presence_of :fecharec
+  validates :motivom, length: { maximum: 5000 }
+  validates :memo1612, length: { maximum: 5000 }
+  validates :oficina, presence: true
+  #_presence_of :oficina
 
-  validate :sitiosm_diferentes
-  def sitiosm_diferentes
-    if llegadam.present? && salidam.present? && id_llegadam == id_salidam
-      errors.add(:llegadam, " debe ser diferente al sitio de salida en migración")
-    end
-  end
 
   validate :fecharec_pasada
   def fecharec_pasada
     if fecharec && fecharec>Date.today
       errors.add(:fecharec, 
                  " la fecha de recepción debe ser en el pasado")
+    end
+  end
+
+  validate :llegada_posterior_a_salida
+  def llegada_posterior_a_salida
+    if fechallegada.present? && fechasalida.present? && 
+        fechallegada < fechasalida
+      errors.add(:fechallegada, 
+                 " debe ser posterior a la fecha de salida")
     end
   end
 
@@ -66,6 +118,64 @@ class Sivel2Sjr::Casosjr < ActiveRecord::Base
         (casosjr.asesor != current_usuario.id))
       errors.add(:id, "Sistematizador solo puede editar sus casos")
     end
+  end
+
+  validate :sitiosm_diferentes
+  def sitiosm_diferentes
+    if llegadam.present? && salidam.present? && id_llegadam == id_salidam
+      errors.add(:llegadam, 
+                 " debe ser diferente al sitio de salida en migración")
+    end
+  end
+
+  # Verifica que un usuario sea de la misma oficina de otro
+  def self.asesor_de_oficina(current_usuario, usuario, oficina)
+    current_usuario.rol == Ability::ROLADMIN ||
+      current_usuario.rol == Ability::ROLDIR ||
+      usuario.rol == Ability::ROLADMIN ||
+      usuario.rol == Ability::ROLDIR ||
+      usuario.oficina_id == oficina.id || oficina.id == 1
+  end
+
+  # Verifica que un usuario edita caso de su oficina
+  def self.asesor_edita_de_su_oficina(current_usuario, oficina)
+    (current_usuario.rol != Ability::ROLSIST &&
+     current_usuario.rol != Ability::ROLCOOR &&
+     current_usuario.rol != Ability::ROLANALI) ||
+    oficina.id == current_usuario.oficina_id
+  end
+
+  before_destroy do
+    no_borra_con_actividades 
+    if errors.present?
+      throw(:abort) 
+    end
+  end
+
+  def no_borra_con_actividades
+    if actividad_casosjr.count > 0
+      errors.add(:base, 'Beneficiario de actividades ('+
+                 actividad_casosjr.pluck(:actividad_id).join(', ') + ')') 
+    end
+  end
+
+
+  # Funciones auxiliares
+
+  def beneficiarios_activos
+    self.victimasjr.where(fechadesagregacion: nil)
+  end
+
+  def actividades_con_beneficiarios_activos_en_asistencia_ids
+    bids = beneficiarios_activos.pluck(:id_persona)
+    Cor1440Gen::Asistencia.joins(:persona).
+      where("msip_persona.id IN (?)", bids).
+      pluck('actividad_id')
+  end
+
+  def actividades_con_caso_ids
+    t = self.actividad_ids |
+      actividades_con_beneficiarios_activos_en_asistencia_ids
   end
 
 end
