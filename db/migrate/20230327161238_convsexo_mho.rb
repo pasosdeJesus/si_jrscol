843fe02bea9e8d7caed59c7cae4160e8a3105872
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-03-29 06:47:49 -0500

    actualiza

--- db/migrate/20230327161238_convsexo_mho.rb
+++ db/migrate/20230327161238_convsexo_mho.rb
@@ -1,6 +1,14 @@
 class ConvsexoMho < ActiveRecord::Migration[7.0]
   def up
     execute <<-SQL
+       -- Deshabilita triggers
+      SET session_replication_role = replica;
+      -- Elimina datos incosistentes
+      DELETE FROM  cor1440_gen_asistencia WHERE actividad_id 
+        NOT IN (SELECT id FROM cor1440_gen_actividad);
+      -- Vuelve a habilitar triggers
+      SET session_replication_role = DEFAULT;
+
       ALTER TABLE msip_persona DROP CONSTRAINT IF EXISTS persona_sexo_check;
       UPDATE msip_persona SET sexo='H' WHERE sexo='M'; 
       UPDATE msip_persona SET sexo='M' WHERE sexo='F';
