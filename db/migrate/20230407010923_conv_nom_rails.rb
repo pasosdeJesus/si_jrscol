46b431b05fbc74a8465ce32e730b096b60a26a3f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-04-08 05:19:24 -0500

    Mejor migracion

--- db/migrate/20230407010923_conv_nom_rails.rb
+++ db/migrate/20230407010923_conv_nom_rails.rb
@@ -1,7 +1,16 @@
 class ConvNomRails < ActiveRecord::Migration[7.0]
-  def change
+  def up
     rename_column :sivel2_sjr_casosjr, :id_salidam, :salidam_id
     rename_column :sivel2_sjr_casosjr, :id_llegadam, :llegadam_id
-    rename_column :sivel2_sjr_respuesta, :id_causaref, :causaref_id
+    if Sivel2Sjr::Respuesta.columns.map(&:name).include?("id_causaref")
+      rename_column :sivel2_sjr_respuesta, :id_causaref, :causaref_id
+    end
+  end
+  def down
+    rename_column :sivel2_sjr_casosjr, :salidam_id, :id_salidam
+    rename_column :sivel2_sjr_casosjr, :llegadam_id, :id_llegadam
+    if Sivel2Sjr::Respuesta.columns.map(&:name).include?("causaref_id")
+      rename_column :sivel2_sjr_respuesta, :causaref, :id_causaref
+    end
   end
 end
