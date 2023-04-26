6d3ea0e8c7e5d48369694bcca43deff8511ea551
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-04-26 10:51:29 -0500

    Etiquetas en persona refactorizado en msip. Closes #869

--- app/models/msip/etiqueta.rb
+++ app/models/msip/etiqueta.rb
@@ -5,10 +5,5 @@ module Msip
   class Etiqueta < ActiveRecord::Base
     include Msip::Concerns::Models::Etiqueta
 
-    has_many :etiqueta_persona, class_name: 'Msip::EtiquetaPersona',
-      foreign_key: 'etiqueta_id', dependent: :delete_all
-    has_many :persona, class_name: 'Msip::Persona',
-      through: :etiqueta_persona
-
   end
 end
