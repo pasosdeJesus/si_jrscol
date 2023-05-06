5bb40d10cb7437398235b2a4b6233f099065a31e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-05-06 14:21:52 -0500

    Refactoriza consactividadcaso en jos19. Closes #873

---
+++ app/models/jos19/consactividadcaso.rb
@@ -0,0 +1,12 @@
+require 'jos19/concerns/models/consactividadcaso'
+
+module Jos19 
+  class Consactividadcaso < ActiveRecord::Base
+    include Jos19::Concerns::Models::Consactividadcaso
+
+    belongs_to :victimasjr,
+      class_name: 'Sivel2Sjr::Victimasjr', foreign_key: 'victima_id', 
+      optional: false
+
+  end
+end
---
+++ db/migrate/20230506183428_consactividadcaso_a_jos19.rb
@@ -0,0 +1,14 @@
+class ConsactividadcasoAJos19 < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      ALTER MATERIALIZED VIEW sivel2_sjr_consactividadcaso RENAME TO
+        jos19_consactividadcaso;
+    SQL
+  end
+  def down
+    execute <<-SQL
+      ALTER MATERIALIZED VIEW sivel2_sjr_consactividadcaso RENAME TO
+        jos19_consactividadcaso;
+    SQL
+  end
+end
