c02cc85cd413df249f81a362affb7ed07185de36
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-08-08 12:33:38 -0500

    Aumenta limites. Closes #913

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < Msip::ApplicationController
   protect_from_forgery with: :exception
 
-  # No requiere autoerización
+  # No requiere autorización
+  if defined?(WEBrick)
+    WEBrick::HTTPRequest.const_set("MAX_URI_LENGTH", 124000)
+  end
 end
 
--- config/initializers/multipart_limit.rb
+++ config/initializers/multipart_limit.rb
@@ -1,1 +1,1 @@
-Rack::Utils.multipart_part_limit = 0
+Rack::Utils.multipart_total_part_limit = 0
