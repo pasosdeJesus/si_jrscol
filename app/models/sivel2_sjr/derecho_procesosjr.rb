83d94b7f730eeebf6cd3636a62d0703c07637a41
Nombre Usuario <correo@usuario.net>
2023-10-27 12:32:55 -0600

    Sintaxis

--- app/models/sivel2_sjr/derecho_procesosjr.rb
+++ app/models/sivel2_sjr/derecho_procesosjr.rb
@@ -3,7 +3,8 @@ module Sivel2Sjr
 
     self.table_name = "sivel2_sjr_derecho_procesosjr"
 
-    foreign_key: "id_proceso", validate: true, optional: false
+    belongs_to :procesosjr, class_name: "Sivel2Sjr::Procesosjr",
+      foreign_key: "id_proceso", validate: true, optional: false
     belongs_to :derecho, class_name: "Sivel2Sjr::Derecho", 
       foreign_key: "derecho_id", validate: true, optional: false
   end
