9b2cca123d9e1e74d6835beea497b031cad19b71
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-12-11 15:06:09 -0500

    Indices y validaciones. Closes #1006

---
+++ db/migrate/20231211155412_indices_unicidad.rb
@@ -0,0 +1,6 @@
+class IndicesUnicidad < ActiveRecord::Migration[7.0]
+  def change
+    add_index :docidsecundario, [:persona_id, :tdocumento_id], unique: true
+    add_index :docidsecundario, [:tdocumento_id, :numero], unique: true
+  end
+end
