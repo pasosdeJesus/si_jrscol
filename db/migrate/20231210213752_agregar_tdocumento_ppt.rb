feac848a01bc42603114baec77a5b5ec1b50c74d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-12-10 21:53:24 -0500

    Homologa PPT como documento de identidad principal o secundario --quitando SIN DOCUMENTO principal. Closes #1003

---
+++ db/migrate/20231210213752_agregar_tdocumento_ppt.rb
@@ -0,0 +1,14 @@
+class AgregarTdocumentoPpt < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      INSERT INTO msip_tdocumento (id, nombre, sigla, formatoregex, 
+        fechacreacion, fechadeshabilitacion, created_at, 
+        updated_at, observaciones, ayuda) VALUES (16, 'PERMISO POR PROTECCIÓN TEMPORAL', 'PPT', '[0-9]*', '2023-12-10', NULL, NULL, NULL, NULL, 'Solo dígitos. Por ejemplo 323948');
+    SQL
+  end
+  def down
+    execute <<-SQL
+      DELETE FROM msip_tdocumento WHERE id=16;
+    SQL
+  end
+end
---
+++ db/migrate/20231210224436_homologa_ppts.rb
@@ -0,0 +1,57 @@
+class HomologaPpts < ActiveRecord::Migration[7.0]
+  def up
+    numprin = 0
+    numsec = 0
+    numprob = 0
+    puts "Id,Sigla,Número de documento,PPT,Razón por la que fue descartado"
+    Msip::Persona.where.not(ppt: nil).where("TRIM(ppt)<>''").each do |p|
+      prob = ""
+      if p.ppt.strip.match(/^[0-9]+$/)
+        if p.ppt.strip != p.numerodocumento
+          if Msip::Persona.where(tdocumento_id: 16).
+              where(numerodocumento: p.ppt.strip).count == 0
+            if Docidsecundario.where(tdocumento_id: 16).
+                where(numero: p.ppt.strip).count == 0
+              if p.tdocumento_id == 11 # SIN DOCUMENTO
+                p.tdocumento_id = 16
+                p.numerodocumento = p.ppt
+                numprin += 1
+              else
+                ds = Docidsecundario.new(
+                  tdocumento_id: 16,
+                  numero: p.ppt.strip,
+                  persona_id: p.id
+                )
+                ds.save
+                numsec += 1
+              end
+            else
+              prob = "Duplicado como documento de identidad secundario de "\
+                "persona con id: " + Docidsecundario.
+                where(tdocumento_id: 16).where(numero:p.ppt.strip).
+                pluck(:persona_id).join(", ")
+            end
+          else
+            prob = "Duplicado como documento de identidad primario de "\
+                "persona con id: " + Msip::Persona.
+                where(tdocumento_id: 16).where(numerodocumento:p.ppt.strip).
+                pluck(:id).join(", ")
+          end
+        else
+          prob = "Documento principal igual al PPT"
+        end
+      else
+        prob = "PPT no consta de sólo números"
+      end
+      if prob != ""
+        puts "#{p.id},#{p.tdocumento.sigla},#{p.numerodocumento},#{p.ppt},#{prob}"
+        numprob += 1
+      end
+    end
+    STDERR.puts "PPT homologado como documento principal de #{numprin} personas"
+    STDERR.puts "PPT homologado como documento secundario de #{numsec} personas"
+    STDERR.puts "PPT descartado de #{numprob} personas"
+  end
+  def down
+  end
+end
