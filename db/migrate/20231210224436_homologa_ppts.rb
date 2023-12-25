42c07534c69e6b8996fec295c28f64660260a691
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-12-25 07:55:57 -0500

    Mejora migracion

--- db/migrate/20231210224436_homologa_ppts.rb
+++ db/migrate/20231210224436_homologa_ppts.rb
@@ -6,20 +6,20 @@ class HomologaPpts < ActiveRecord::Migration[7.0]
     puts "Id,Sigla,Número de documento,PPT,Razón por la que fue descartado"
     Msip::Persona.where.not(ppt: nil).where("TRIM(ppt)<>''").each do |p|
       prob = ""
-      if p.ppt && p.ppt.strip.match(/^[0-9]+$/)
-        if p.ppt.strip != p.numerodocumento
+      if p[:ppt] && p[:ppt].strip.match(/^[0-9]+$/)
+        if p[:ppt].strip != p.numerodocumento
           if Msip::Persona.where(tdocumento_id: 16).
-              where(numerodocumento: p.ppt.strip).count == 0
+              where(numerodocumento: p[:ppt].strip).count == 0
             if Docidsecundario.where(tdocumento_id: 16).
-                where(numero: p.ppt.strip).count == 0
+                where(numero: p[:ppt].strip).count == 0
               if p.tdocumento_id == 11 # SIN DOCUMENTO
                 p.tdocumento_id = 16
-                p.numerodocumento = p.ppt
+                p.numerodocumento = p[:ppt]
                 numprin += 1
               else
                 ds = Docidsecundario.new(
                   tdocumento_id: 16,
-                  numero: p.ppt.strip,
+                  numero: p[:ppt].strip,
                   persona_id: p.id
                 )
                 ds.save
@@ -28,13 +28,13 @@ class HomologaPpts < ActiveRecord::Migration[7.0]
             else
               prob = "Duplicado como documento de identidad secundario de "\
                 "persona con id: " + Docidsecundario.
-                where(tdocumento_id: 16).where(numero:p.ppt.strip).
+                where(tdocumento_id: 16).where(numero:p[:ppt].strip).
                 pluck(:persona_id).join(", ")
             end
           else
             prob = "Duplicado como documento de identidad primario de "\
                 "persona con id: " + Msip::Persona.
-                where(tdocumento_id: 16).where(numerodocumento:p.ppt.strip).
+                where(tdocumento_id: 16).where(numerodocumento:p[:ppt].strip).
                 pluck(:id).join(", ")
           end
         else
@@ -44,7 +44,7 @@ class HomologaPpts < ActiveRecord::Migration[7.0]
         prob = "PPT no consta de sólo números"
       end
       if prob != ""
-        puts "#{p.id},#{p.tdocumento.sigla},#{p.numerodocumento},#{p.ppt},#{prob}"
+        puts "#{p.id},#{p.tdocumento.sigla},#{p.numerodocumento},#{p[:ppt]},#{prob}"
         numprob += 1
       end
     end
@@ -53,5 +53,6 @@ class HomologaPpts < ActiveRecord::Migration[7.0]
     STDERR.puts "PPT descartado de #{numprob} personas"
   end
   def down
+    raise IrreversibleMigration
   end
 end
