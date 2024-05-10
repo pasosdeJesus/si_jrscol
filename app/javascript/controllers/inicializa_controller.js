bae4bdb79c470ccd07c8afaff08cf41b2325b3a1
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-05-10 13:22:15 -0500

    Vuelve a dejar inicializacion js

--- app/javascript/controllers/inicializa_controller.js
+++ app/javascript/controllers/inicializa_controller.js
@@ -1,10 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 // Inicializa lo necesario tras cargar página
 
+import Msip__Motor from './msip/motor'
 import TomSelect from 'tom-select';
 
 export default class extends Controller {
 
+  // Este se ejecuta cuando se presenta el body de la página,
+  // Pero eso podría ser antes de que se complete la carga de
+  //   javascript/application.js
   static targets = [ ]
 
   connect() {
