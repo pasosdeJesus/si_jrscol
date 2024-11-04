52b515ae410156bbd118d6b2c600a25f0ad712c2
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-11-04 01:05:59 -0500

    doc

--- app/models/msip/persona.rb
+++ app/models/msip/persona.rb
@@ -2,6 +2,7 @@ require 'sivel2_gen/concerns/models/persona'
 require 'cor1440_gen/concerns/models/persona'
 
 module Msip
+  # Datos de un beneficiario
   class Persona < ActiveRecord::Base
     include Sivel2Gen::Concerns::Models::Persona
     include Cor1440Gen::Concerns::Models::Persona
