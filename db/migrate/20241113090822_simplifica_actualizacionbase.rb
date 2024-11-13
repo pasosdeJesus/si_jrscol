d986480b265ba394d78e969b9bcf4008b9a64304
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-11-13 04:49:58 -0500

    Elimina talbas no usadas. Avanza #1099

---
+++ db/migrate/20241113090822_simplifica_actualizacionbase.rb
@@ -0,0 +1,8 @@
+class SimplificaActualizacionbase < ActiveRecord::Migration[7.2]
+  def up
+    drop_table :sivel2_sjr_actualizacionbase
+    drop_table :sivel2_sjr_instanciader
+    drop_table :sivel2_sjr_mecanismoder
+    drop_table :sivel2_sjr_resagresion
+  end
+end
