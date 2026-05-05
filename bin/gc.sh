99b6cb5acb5845ea0341f220362ef99e9225881c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-05-05 09:11:57 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -40,8 +40,8 @@ if (test "$SINAC" != "1") then {
     echo "Eliminando $rutapore/*"
     rm -rf $rutapore/*
   } fi;
-  NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update --all
   NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update --bundler
+  NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update --all
   if (test "$?" != "0") then {
     exit 1;
   } fi;
